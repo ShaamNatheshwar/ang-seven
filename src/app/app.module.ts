99ec6817e6b3ea5f468d0df8cf0ce9e482be1291
ssn <shaamnatheshwar@gmail.com>
2023-05-16 08:47:03 +0530

    done with 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BasicHighlight } from './basic-highlight/basic-highlight.directive';
 import { BetterHighlightDirectiveDirective } from './better-highlight/better-highlight.directive.directive';
+import { UnlessDirective } from './unless.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     BasicHighlight,
-    BetterHighlightDirectiveDirective
+    BetterHighlightDirectiveDirective,
+    UnlessDirective
   ],
   imports: [
     BrowserModule
