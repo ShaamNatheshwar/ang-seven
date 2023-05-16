84efa491a207297489417a1e4099244fe4e1fdde
ssn <shaamnatheshwar@gmail.com>
2023-05-16 07:46:46 +0530

    done with creating directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BasicHighlight } from './basic-highlight/basic-highlight.directive';
+import { BetterHighlightDirectiveDirective } from './better-highlight/better-highlight.directive.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHighlight,
+    BetterHighlightDirectiveDirective
   ],
   imports: [
     BrowserModule
